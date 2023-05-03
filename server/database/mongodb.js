01ddfea3950668209a96470718a03367ce22518c
nizam-chenath <nizamstudymajliz@gmail.com>
2023-05-03 13:41:35 +0530

    strictquery commented

--- server/database/mongodb.js
+++ server/database/mongodb.js
@@ -6,7 +6,7 @@ async function connect () {
     const password = process.env.MONGO_DB_PASSWORD
     const url = process.env.MONGO_DB_URL
 
-    mongoose.set('strictQuery', false)
+    // mongoose.set('strictQuery', false)
     
     await mongoose.connect(`mongodb+srv://${username}:${password}@${url}/?retryWrites=true&w=majority`).then(()=>{
         console.log("mongoDB connection is successful")
