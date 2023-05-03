663142aeab5ad981a6b1476aa230e7cd8e7612bc
nizam-chenath <nizamstudymajliz@gmail.com>
2023-05-03 15:00:57 +0530

    mongodb url env file changed

--- server/database/mongodb.js
+++ server/database/mongodb.js
@@ -2,13 +2,13 @@ import mongoose from 'mongoose'
 
 async function connect () {
 
-    const username = process.env.MONGO_DB_USERNAME
-    const password = process.env.MONGO_DB_PASSWORD
-    const url = process.env.MONGO_DB_URL
+    // const username = process.env.MONGO_DB_USERNAME
+    // const password = process.env.MONGO_DB_PASSWORD
+    // const url = process.env.MONGO_DB_URL
 
     mongoose.set('strictQuery', true)
     
-    await mongoose.connect(`mongodb+srv://${username}:${password}@${url}/?retryWrites=true&w=majority`).then(()=>{
+    await mongoose.connect(process.env.MONGO_DB_URL).then(()=>{
         console.log("mongoDB connection is successful")
     }).catch((err) =>{
         console.log(err)
